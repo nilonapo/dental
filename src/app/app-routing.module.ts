79a1bbb84e0f03dcf151c7267969f3d4a7448b8e
OAKSMITH\nilonapo <nap.nilo@gmail.com>
2018-10-17 23:31:56 +0800

    implementing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent}
+  { path: 'services/:id', component: ServicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
